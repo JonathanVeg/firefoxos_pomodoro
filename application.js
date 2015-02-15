7c0954c7649ea24fc6415801546bbf5244ef0986
Jonathan Silva <jonathan.jgs@gmail.com>
2015-02-15 01:41:13 -0200

    ajuste de mudanca de cor no modo fullscreen

--- application.js
+++ application.js
@@ -97,7 +97,8 @@ window.onload = function() {
 					$(".message").html("Agora é hora de relaxar. Tente fazer qualquer coisa para relaxar a mente. <br> Dica: alongue-se um pouco.");
 				}
 			}
-			if (current_step == "p" && show_fullscreen){
+			if (show_fullscreen){
+				$("#cur_time").css("background-color", getColorForPercentage(current_timer / timer_pomodoro));
 				$("#fullscreen").show();
 			}else{
 				$("#fullscreen").hide();
@@ -322,4 +323,29 @@ window.onload = function() {
 	$("#panel_settings").hide();
 	$("#panel_infos").hide();
 	setLanguage("");
+
+	var percentColors = [
+	{ pct: 0.0, color: { r: 0x00, g: 0xff, b: 0 } },
+	{ pct: 0.5, color: { r: 0xff, g: 0xff, b: 0 } },
+	{ pct: 1.0, color: { r: 0xff, g: 0x00, b: 0 } } ];
+
+	var getColorForPercentage = function(pct) {
+		for (var i = 1; i < percentColors.length - 1; i++) {
+			if (pct < percentColors[i].pct) {
+				break;
+			}
+		}
+		var lower = percentColors[i - 1];
+		var upper = percentColors[i];
+		var range = upper.pct - lower.pct;
+		var rangePct = (pct - lower.pct) / range;
+		var pctLower = 1 - rangePct;
+		var pctUpper = rangePct;
+		var color = {
+			r: Math.floor(lower.color.r * pctLower + upper.color.r * pctUpper),
+			g: Math.floor(lower.color.g * pctLower + upper.color.g * pctUpper),
+			b: Math.floor(lower.color.b * pctLower + upper.color.b * pctUpper)
+		};
+		return 'rgb(' + [color.r, color.g, color.b].join(',') + ')';
+	}  
 }
